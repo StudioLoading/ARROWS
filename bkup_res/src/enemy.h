4fc74eed6aa4ca0449c831f79d02ca0f52ac6e00
Studio Loading <studioloadingproductions@gmail.com>
2021-02-01 10:41:14 +0100

    Level 1.2 update

--- bkup_res/src/enemy.h
+++
@@ -1,6 +0,0 @@
-#ifndef TILES_enemy_H
-#define TILES_enemy_H
-#include "TilesInfo.h"
-extern unsigned char bank_enemy;
-extern struct TilesInfo enemy;
-#endif
--- bkup_res/src/platform.h
+++
@@ -1,6 +0,0 @@
-#ifndef TILES_platform_H
-#define TILES_platform_H
-#include "TilesInfo.h"
-extern unsigned char bank_platform;
-extern struct TilesInfo platform;
-#endif
