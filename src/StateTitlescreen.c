5432c7ce555949b4d4eaa1fc5f2e59d7b3356a11
Studio Loading <studioloadingproductions@gmail.com>
2021-08-15 22:28:24 +0200

    Titlescreen fixed, still unpolished

--- src/StateTitlescreen.c
+++ src/StateTitlescreen.c
@@ -3,7 +3,9 @@
 #include "../res/src/tilestitlescreen.h"
 #include "../res/src/maptitlescreen.h"
 #include "../res/src/archer.h"
-#include "../res/src/logo.h"
+#include "../res/src/font.h"
+#include "../res/src/windowpushstart.h"
+
 
 #include <gb/gb.h>
 
@@ -20,15 +22,12 @@
 
 #include "custom_datas.h"
 
-
 const UINT8 collision_tiles_titlescreen[] = {1,0};
 const UINT16 bg_palette_titlescreen[] = {PALETTE_FROM_HEADER(tilestitlescreen)};
 
-struct Sprite* sprite_logo_0;
-struct Sprite* sprite_logo_1;
-struct Sprite* sprite_logo_2;
-
 UINT8 wait_titlescreen = 255U;
+
+void ShowPushStart();
 
 const UINT16 sprites_palette_titlescreen[] = {
 	PALETTE_INDEX(archer, 0),
@@ -47,10 +46,12 @@ void Start_StateTitlescreen() {
 	SetPalette(BG_PALETTE, 0, 8, bg_palette_titlescreen, 2);
 	
 	SPRITES_8x16;
-	//SPRITES_32x32;
-	SpriteManagerLoad(SpriteLogo);
-	//scroll_target = SpriteManagerAdd(SpriteLogo, 0, 0);
 	SHOW_SPRITES;
+	
+	//WINDOW	
+	INIT_FONT(font, PRINT_WIN);
+	INIT_CONSOLE(font, 10, 2);
+	ShowPushStart();
 	
 	//SOUND
 	NR52_REG = 0x80; //Enables sound, you should always setup this first
@@ -64,33 +65,28 @@ void Start_StateTitlescreen() {
 
 void Update_StateTitlescreen() {	
 			
-	switch(wait_titlescreen){
-		case 255u:
-			sprite_logo_0 = SpriteManagerAdd(SpriteLogo, 0, 0);
-			struct LogoInfo* logo_data = (struct LogoInfo*)sprite_logo_0->custom_data;
-			logo_data->frame = 0;
-			logo_data->setup = 1;
-		break;
-		case 254u:
-			sprite_logo_1 = SpriteManagerAdd(SpriteLogo, (UINT16) 4u << 3, 0);
-			struct LogoInfo* logo_data_1 = (struct LogoInfo*)sprite_logo_1->custom_data;
-			logo_data_1->frame = 1;
-			logo_data_1->setup = 1;
-		break;
-		case 253u:
-			sprite_logo_2 = SpriteManagerAdd(SpriteLogo,  (UINT16) 8u << 3, 0);
-			struct LogoInfo* logo_data_2 = (struct LogoInfo*)sprite_logo_2->custom_data;
-			logo_data_2->frame = 2;
-			logo_data_2->setup = 1;
-		break;
-	}
-	
 	if(KEY_TICKED(J_START)){
 		SetState(StateGame);	
 	}
 	
 	if(wait_titlescreen > 252u){
 		wait_titlescreen -= 1u;
+	}else{
+		if (wait_titlescreen > 60u){
+			wait_titlescreen = 50u;
+		}
+		//loop wait_titlescreen up to 120
+		wait_titlescreen -= 1u;
+		if (wait_titlescreen == 0u){
+			wait_titlescreen = 60u;
+			return;
+		}
+		if(wait_titlescreen == 30u){
+			HIDE_WIN;
+		}else if(wait_titlescreen == 59u){
+			SHOW_WIN;
+		}	
+		
 	}
 	/*
 		FadeIn();
@@ -115,4 +111,13 @@ void Update_StateTitlescreen() {
 		return;
 	}*/
 	
+}
+
+void ShowPushStart(){
+	WX_REG = 4u;
+	WY_REG = 136u;
+	InitWindow(0, 0, &windowpushstart);
+	PRINT_POS(0,0);
+	Printf("      PUSH START     ");
+	SHOW_WIN;
 }
