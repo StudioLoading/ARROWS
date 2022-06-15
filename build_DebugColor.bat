a4ff3d91c45a2ec16afd1304872d003998fe21be
StudioLoading <studioloadingproductions@gmail.com>
2022-06-15 14:57:22 +0200

    camerafocus with empty frame

---
+++ build_DebugColor.bat
@@ -0,0 +1,5 @@
+cd src
+%ZGB_PATH%\..\env\make-3.81-bin\bin\make run BUILD_TYPE=DebugColor
+cp /Y C:\GB\ZGB-2021.3\ZGB-template-master\camerafocus.gbr.c C:\GB\ZGB-2021.3\ZGB-template-master\DebugColor\res\sprites\camerafocus.gbr.c
+cd ..
+pause
